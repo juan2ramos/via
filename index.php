2ebf89c23d8e25f31037bb4bbf13d51967f7e5dc
juan2ramos <juan2ramos@gmail.com>
2014-01-30 17:42:47 -0500

    footer

--- index.php
+++ index.php
@@ -97,6 +97,9 @@
         <section >
 
         </section>
+        <footer>
+            
+        </footer>
     </body>
     <!-- JavaScript -->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
