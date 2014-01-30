fe38aa5a178a8280e88faeef285379ea1b140719
juan2ramos <juan2ramos@gmail.com>
2014-01-30 15:16:01 -0500

    organizacion carpetas

---
+++ index.php
@@ -0,0 +1,67 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+<html lang="es">
+    <head> 
+
+        <title>Via</title>
+
+        <!-- Meta -->
+        <meta charset="utf-8">
+        <meta name="author" content="" />       
+        <meta name="description" content="Inicio" />       
+        <!-- Si hay diseño responsive
+        <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1" />
+        -->
+
+        <script src="js/prefixfree.min.js"></script>
+        <!-- Estilos -->
+        <link rel="stylesheet" href="css/normalize.css" />
+        <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/style.css" />
+
+
+    </head>
+    <body>
+        <header>
+            
+            <div class="carousel">
+                <img src="images/brasil_pais_invitado.png" alt="" />
+            </div>
+            <div class="background-left"></div>
+            <div  class="content-head">
+                <div class="container-nav">
+                    <ul class="menus">
+                        <li><a href="#"><span class="activate" >es</span></a></li>
+                        <li><a href="#"><span>en</span></a></li>
+                    </ul>
+                    <div id="logo"><img src="images/logo-festival-teatro.svg" alt="XIV Festival iberoamericano de teatro de bogotá" /></div>
+                    <nav>                  
+                        
+                            <a href="#" class="activate">Programación<span>Agenda</span></a>
+                            <a href="#">Artistas<span>Artists</span></a>
+                            <a href="#">Portafolios<span>Portfolios</span></a>
+                            <a href="#">Programadores<span>Promoters</span></a>
+                            <a href="#">Info Bogotá<span>Bogotá</span></a>
+                            <a href="#">Contacto<span>Contact</span></a>
+                        
+                    </nav>
+                    <ul class="menus redes">
+                        <li><a href="#">f</a></li>
+                        <li><a href="#">t</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        
+        <section >
+
+        </section>
+    </body>
+    <!-- JavaScript -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="js/script.js"></script>
+</html>
