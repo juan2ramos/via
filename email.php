7200d452ff922fef7ca0512903df09686424d8ec
juan2ramos <juan2ramos@gmail.com>
2014-02-01 10:08:31 -0500

    mail

--- email.php
+++ email.php
@@ -7,33 +7,26 @@ echo "jan";
 	require 'include/PHPMailer/PHPMailerAutoload.php';
 
 	$mail = new PHPMailer;
-
-	$mail->isSMTP();                                      // Set mailer to use SMTP
-	$mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup server
-	$mail->SMTPAuth = true;                               // Enable SMTP authentication
-	$mail->Username = 'juan2ramos@gmail.com';                            // SMTP username
-	$mail->Password = 'JcRpYjErT1212';                           // SMTP password
-	$mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
+	$mail->IsSendmail();
+	$mail->FromName ='jkjkjkj' ;
 
 	$mail->From = 'juan2ramos@gmail.com';
-	$mail->FromName = 'Mailer';
-	$mail->addAddress('jramosp1@ucentral.edu.co', 'Josh Adams');  // Add a recipient
+	$mail->Subject = 'Boletin de Variedades #1 ';
+	$mail->MsgHTML('Mensaje con HTML');
+	$template = "sdfsdf";
+	$mail->Body = $template;
 
+	
+	    $mail->AddAddress('juan2ramos@gmail.com', '');
 
-	$mail->WordWrap = 50;                                 // Set word wrap to 50 characters
-	       // Add attachments
-	    // Optional name
-	$mail->isHTML(true);                                  // Set email format to HTML
+	    if (!$mail->Send()) {
+	        echo $mail->ErrorInfo;
+	    } else { 
+	    }
+	    $mail->ClearAddresses();
+	    
+	
 
-	$mail->Subject = 'Here is the subject';
-	$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-	$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-	if(!$mail->send()) {
-	   echo 'Message could not be sent.';
-	   echo 'Mailer Error: ' . $mail->ErrorInfo;
-	   exit;
-	}
 
 	 $arrayMsj['success'] = TRUE;
      $arrayMsj['url'] = '/administracion/principalAdmin';
