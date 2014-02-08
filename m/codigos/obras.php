df4f3798b6149cf72cba8dfcf2d3807053b913db
master08 <cesarvalencia11@gmail.com>
2014-02-08 11:42:20 -0500

    enlaces

--- m/codigos/obras.php
+++ m/codigos/obras.php
@@ -27,7 +27,7 @@ table{
 <script>
 function regreso(){
 	alert("ssss");
-	 window.parent.refrescar();
+	// window.parent.refrescar();
 	// window.frameElement.ownerDocument.parentWindow.refrescar();
 	 window.parent.location="http://redlat.org/via/m/index.php?modo=inscripciones&mode=paso_3muestra&id_usuario=<?=$_GET["id_usuario"]?>&area=<?=$_GET["area"]?>&id_grupo=<?=$_GET["item"]?>";
 	}
