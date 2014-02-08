af6fd7867aab14f31b902e8ce6234d57dfe27ab1
master08 <cesarvalencia11@gmail.com>
2014-02-08 10:49:31 -0500

    ajuste de enlace en el paso 2 del formulario de artistas

--- m/CCB/paso_2.php
+++ m/CCB/paso_2.php
@@ -126,7 +126,7 @@ footer{
           <tr>
             <td width="310" rowspan="7" align="left" valign="top" ><table width="96%" border="0" cellspacing="0" cellpadding="0" id="imagenGrupo">
               <tr>
-                <td align="center" valign="middle" style="height:400px;"><iframe scrolling="no" width="100%" height="400px" frameborder="0" src="http://reglat.org/via/m/codigos/artista_imagen.php?item=<?=$frm["id_grupo"]?>&area=<?=$frm["area"]?>"></iframe></td>
+                <td align="center" valign="middle" style="height:400px;"><iframe scrolling="no" width="100%" height="400px" frameborder="0" src="http://redlat.org/via/m/codigos/artista_imagen.php?item=<?=$frm["id_grupo"]?>&area=<?=$frm["area"]?>"></iframe></td>
               </tr>
             </table></td>
             <td width="235" align="left" valign="top" class="colorTexto">(*) Nombre del grupo o del artista:</td>
@@ -336,7 +336,7 @@ footer{
           <tr>
             <td height="88" align="left" valign="top">&nbsp;</td>
             <td align="left" valign="top" class="colorTexto" >(*) Trayectoria en pdf:</td>
-            <td colspan="2"  valign="top" align="center"><iframe width="355px" height="170px" src="http://reglat.org/via/m/codigos/artistas_trayectoria.php?item=<?=$frm["id_grupo"]?>&area=<?=$frm["area"]?>" scrolling="no" frameborder="0"></iframe></td>
+            <td colspan="2"  valign="top" align="center"><iframe width="355px" height="170px" src="http://redlat.org/via/m/codigos/artistas_trayectoria.php?item=<?=$frm["id_grupo"]?>&area=<?=$frm["area"]?>" scrolling="no" frameborder="0"></iframe></td>
           </tr>
           <tr>
             <td align="left" valign="top">&nbsp;</td>
@@ -385,7 +385,7 @@ footer{
             Indique el nombre y documento de las dos personas que lo(s) representar&aacute;(n) en la Rueda de Negocios.<br />
             <br />
             <div style="width:100%; height:140px">
-            <iframe width="100%" height="140px" frameborder="0" scrolling="no" src="http://reglat.org/via/m/codigos/vinculados.php?item=<?=$frm["id_grupo"]?>&area=<?=$frm["area"]?>&paso=1"></iframe>
+            <iframe width="100%" height="140px" frameborder="0" scrolling="no" src="http://redlat.org/via/m/codigos/vinculados.php?item=<?=$frm["id_grupo"]?>&area=<?=$frm["area"]?>&paso=1"></iframe>
             </div></td>
           </tr>
           
