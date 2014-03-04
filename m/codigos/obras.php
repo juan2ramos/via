ff7183d6689865964f53aaf6b9d6cc76c624a882
master08 <cesarvalencia11@gmail.com>
2014-03-03 19:05:04 -0500

    falta de una tilde en una I

--- m/codigos/obras.php
+++ m/codigos/obras.php
@@ -149,7 +149,7 @@ if($frm["paso"]==1){
     <br>
     <br>
     Rese&ntilde;a (Ingl&eacute;s) *<br />
-    &quot;Favor usar en el texto en ingl&eacute;s la t&iacute;lde (&acute;) en lugar de el ap&oacute;strofo (') para las palabras que así lo requieran&quot;    <br>
+    &quot;Favor usar en el texto en ingl&eacute;s la t&iacute;lde (&acute;) en lugar de el ap&oacute;strofo (') para las palabras que as&iacute; lo requieran&quot;    <br>
         <textarea rows="10" cols="50" name="en_resena"><?=nvl($obra["en_resena"])?>
         </textarea>
         <br>
