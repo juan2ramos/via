30c08f5a7279f95e4f258261debed9cb6ea37e1f
master08 <cesarvalencia11@gmail.com>
2014-03-17 18:01:56 -0500

    ajuste agendamiento

--- m/plantillas/agenda.php
+++ m/plantillas/agenda.php
@@ -1632,7 +1632,7 @@ function listar_grupos($frm){
 		if($gm["id_grupo_musica"]!=""){$tipo="musica";$tipoTxt="Música";$id=$gm["id_grupo_musica"];}
 		elseif($gm["id_grupo_danza"]!=""){$tipo="danza";$tipoTxt="Danza";$id=$gm["id_grupo_danza"];}
 		elseif($gm["id_grupo_teatro"]!=""){$tipo="teatro";$tipoTxt="Teatro";$id=$gm["id_grupo_teatro"];}
-		echo $tipo_anterior=$tipo;
+		$tipo_anterior=$tipo;
 		$qImagen=$db->sql_query("
 			SELECT g.id,g.nombre
 			FROM grupos_$tipo g
@@ -1640,7 +1640,7 @@ function listar_grupos($frm){
 		");
 		if($grupo=$db->sql_fetchrow($qImagen)){
 		//codigo nuevo 
-		 // if($grupo["id"]!=1555){
+		  if($grupo["id"]!=1555){
 			$link=simple_me($ME) . "?mercado=".$CFG->mercado."&modo=agenda&mode=agenda_artista&tipo=$tipo&id_artista=$grupo[id]&id_mercado=$frm[id_mercado]&act=0&id_promotor=$_GET[id_promotor]";
 			echo "<table class='items' border='0' cellspacing='0' cellpadding='0' ><tr><td>";
 			echo "<a style=\"text-align:left; border:none; background:none; cursor:pointer;\" onclick=\"artistas('".$link."')\" title=\"Ver agenda y perfil del artista\">";
@@ -1648,7 +1648,7 @@ function listar_grupos($frm){
 			echo "</a>&nbsp;";
 			echo "</td></tr></table>";
 			$j++;
-		   //}
+		   }
 		}
 	}
 	echo "</tr></td></table></div>"; 
