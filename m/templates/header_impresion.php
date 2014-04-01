cf9e3e2232af7514aa1e966de4ee32d0e18aa65a
master08 <cesarvalencia11@gmail.com>
2014-04-01 11:56:20 -0500

    cambio de cabecera

--- m/templates/header_impresion.php
+++ m/templates/header_impresion.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-<title>Altavoz2013</title>
+<title>VIA2014</title>
 <link href="../estilos.css" rel="stylesheet" type="text/css" />
 <style type="text/css">
 <!--
