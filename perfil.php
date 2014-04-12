9b0b507fe4bf063affb012349ea5d933680c9f07
master08 <cesarvalencia11@gmail.com>
2014-04-12 18:07:15 -0500

    ajuste

--- perfil.php
+++ perfil.php
@@ -76,7 +76,8 @@
 					}
 				    echo utf8_encode($enlace)?><hr></div>
                   <div id="trayectoria" class="text"><strong>trayectoria: </strong><a href="http://circulart.org/admin/fileFS.php?table=grupos_teatro&field=trayectoria&id=<?=$gt["id"]?>">ver</a><hr></div>
-                  <div id="resena" class="text pequeno"><strong> RESEÑA: </strong><hr> <?=utf8_encode($gt["resena_corta"])?></div>
+                  <div><strong> RESEÑA: </strong></div>
+                  <div id="resena" class="text pequeno"><hr> <?=utf8_encode($gt["resena_corta"])?></div>
                   <div id="resena" class="text pequeno"><br><br>/ <?=utf8_encode($gt["en_resena_corta"])?></div>
                   <div id="cont_redes">
                     <div id="redes">
